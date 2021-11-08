0226c0aeb47848dde97b9eb3322c28210edeb711
samwiseg0 <2241731+samwiseg0@users.noreply.github.com>
2021-11-07 21:29:07 -0500

    Init

---
+++ scripts/set_lights_brightness.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#brightness=$1
+
+/usr/bin/curl "http://192.168.20.44/win&A=$1"
---
+++ scripts/set_lights_preset.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Light Preset=$1
+# TORCH = 1
+# IDLE = 2
+# PAUSED = 3
+# PRINTING = 4
+# HEATING = 5
+# PRINT_COMP = 6
+# UNKN = 7
+
+/usr/bin/curl "http://192.168.20.44/win&PL=$1"
