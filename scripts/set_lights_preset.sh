ac6ea62755dc8836eff1eb6b0a8165382b7a0750
samwiseg0 <2241731+samwiseg0@users.noreply.github.com>
2022-02-09 22:02:55 -0500

    Auto-commit on change (2022-02-09 22:02:55)

--- scripts/set_lights_preset.sh
+++ scripts/set_lights_preset.sh
@@ -2,13 +2,15 @@
 
 # Light Preset=$1
 
-# TORCH = 1
+# PRINT_READY = 1
 # IDLE = 2
 # PAUSED = 3
 # PRINTING = 4
 # HEATING = 5
 # PRINT_COLD = 6
 # PRINT_HOT = 7
+# PRINT_CANCEL = 8
+# BUSY = 9
 
 /usr/bin/curl "http://192.168.20.44/win&PL=$1"
 /usr/bin/curl "http://192.168.20.251/win&PL=$1"
